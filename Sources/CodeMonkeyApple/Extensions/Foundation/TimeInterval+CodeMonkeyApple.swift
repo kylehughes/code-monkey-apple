f5cfb79d7460d23b424324a549f89b1ac5f5691d
Kyle Hughes <me@kylehugh.es>
2021-02-27 13:22:01 -0800

    Add warning comments to large TimeInterval helper functions

--- Sources/CodeMonkeyApple/Extensions/Foundation/TimeInterval+CodeMonkeyApple.swift
+++ Sources/CodeMonkeyApple/Extensions/Foundation/TimeInterval+CodeMonkeyApple.swift
@@ -10,18 +10,22 @@ import Foundation
 extension TimeInterval {
     // MARK: Public Static Interface
     
+    /// - Warning: This is unmoored from reality. Consider using higher abstractions like `Calendar`.
     public static func day() -> TimeInterval {
         days(1)
     }
     
+    /// - Warning: This is unmoored from reality. Consider using higher abstractions like `Calendar`.
     public static func days(_ days: Double) -> TimeInterval {
         hours(24) * days
     }
     
+    /// - Warning: This is unmoored from reality. Consider using higher abstractions like `Calendar`.
     public static func hour() -> TimeInterval {
         hours(1)
     }
     
+    /// - Warning: This is unmoored from reality. Consider using higher abstractions like `Calendar`.
     public static func hours(_ hours: Double) -> TimeInterval {
         minutes(60) * hours
     }
@@ -34,10 +38,12 @@ extension TimeInterval {
         milliseconds / 1000
     }
     
+    /// - Warning: This is unmoored from reality. Consider using higher abstractions like `Calendar`.
     public static func minutes() -> TimeInterval {
         minutes(1)
     }
     
+    /// - Warning: This is unmoored from reality. Consider using higher abstractions like `Calendar`.
     public static func minutes(_ minutes: Double) -> TimeInterval {
         seconds(60) * minutes
     }
