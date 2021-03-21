33a11d7f611ab25a18febc2d623c97a332d7a469
Kyle Hughes <me@kylehugh.es>
2021-03-21 15:31:31 -0700

    Add function to easily describe a String

---
+++ Sources/CodeMonkeyApple/Global/Global+Logging.swift
@@ -0,0 +1,10 @@
+//
+//  Global+Logging.swift
+//  CodeMonkeySwift
+//
+//  Created by Kyle Hughes on 3/21/21.
+//
+
+public func describe(_ thing: Any) -> String {
+    String(describing: thing)
+}
