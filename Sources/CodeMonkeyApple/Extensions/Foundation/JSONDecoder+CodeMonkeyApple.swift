a54ffcfb71b7e26215be72c329d302baa445146f
Kyle Hughes <me@kylehugh.es>
2021-06-12 01:34:27 -0700

    Add default JSON decoder and encoder

---
+++ Sources/CodeMonkeyApple/Extensions/Foundation/JSONDecoder+CodeMonkeyApple.swift
@@ -0,0 +1,14 @@
+//
+//  JSONDecoder+CodeMonkeyApple.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 6/12/21.
+//
+
+import Foundation
+
+// MARK: - Constants
+
+extension JSONDecoder {
+    public static let `default` = JSONDecoder()
+}
---
+++ Sources/CodeMonkeyApple/Extensions/Foundation/JSONEncoder+CodeMonkeyApple.swift
@@ -0,0 +1,14 @@
+//
+//  JSONEncoder+CodeMonkeyApple.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 6/12/21.
+//
+
+import Foundation
+
+// MARK: - Constants
+
+extension JSONEncoder {
+    public static let `default` = JSONEncoder()
+}
