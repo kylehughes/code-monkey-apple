bcaf3ed5571efd54a0a0f39465ee703269d51cf6
Kyle Hughes <me@kylehugh.es>
2021-08-03 22:00:43 -0700

    Add ReverseDomainName constant for es.computertechniqu

--- Sources/CodeMonkeyApple/Domain Name/ReverseDomainName.swift
+++ Sources/CodeMonkeyApple/Domain Name/ReverseDomainName.swift
@@ -16,6 +16,7 @@ public struct ReverseDomainName: Equatable, Hashable, Codable, RawRepresentable 
 
 extension ReverseDomainName {
     public static let es = ReverseDomainName(rawValue: "es")
+    public static let es_computertechniqu = es.adding(subdomain: "computertechniqu")
     public static let es_kylehugh = es.adding(subdomain: "kylehugh")
     public static let es_kylehugh_codeMonkey = es_kylehugh.adding(subdomain: "code-monkey")
     public static let es_kylehugh_codeMonkey_apple = es_kylehugh_codeMonkey.adding(subdomain: "apple")
