208cf3a2481dfb434ddcca93396dbc2333861899
Kyle Hughes <me@kylehugh.es>
2021-08-03 23:56:10 -0700

    Split out UserDefaultsKey portion of FeedbackGenerator into separate file

---
+++ Sources/CodeMonkeyApple/Feedback Generator/FeedbackGenerator+UserDefaultsKey.swift
@@ -0,0 +1,46 @@
+//
+//  FeedbackGenerator+UserDefaultsKey.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 8/3/21.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+extension FeedbackGenerator {
+    // MARK: Public Initialization
+    
+    public convenience init(isDisabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
+        self.init(
+            isDisabledProvider: {
+                userDefaults.getValue(for: isDisabledKey)
+            }
+        )
+    }
+    
+    public convenience init(isEnabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
+        self.init(
+            isEnabledProvider: {
+                userDefaults.getValue(for: isEnabledKey)
+            }
+        )
+    }
+    
+    // MARK: Public Instance Interface
+    
+    public func setIsDisabled(basedOn isDisabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
+        setIsDisabled {
+            userDefaults.getValue(for: isDisabledKey)
+        }
+    }
+    
+    public func setIsEnabled(basedOn isEnabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
+        setIsEnabled {
+            userDefaults.getValue(for: isEnabledKey)
+        }
+    }
+}
+
+#endif
--- Sources/CodeMonkeyApple/Feedback Generator/FeedbackGenerator.swift
+++ Sources/CodeMonkeyApple/Feedback Generator/FeedbackGenerator.swift
@@ -1,6 +1,6 @@
 //
 //  FeedbackGenerator.swift
-//  Rank Things
+//  CodeMonkeyApple
 //
 //  Created by Kyle Hughes on 5/30/21.
 //
@@ -236,42 +236,6 @@ public final class FeedbackGenerator {
             prepare(notification)
         case let .selection(selection):
             prepare(selection)
-        }
-    }
-}
-
-// MARK: - App-Specific Extensions
-
-extension FeedbackGenerator {
-    // MARK: Public Initialization
-    
-    public convenience init(isDisabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
-        self.init(
-            isDisabledProvider: {
-                userDefaults.getValue(for: isDisabledKey)
-            }
-        )
-    }
-    
-    public convenience init(isEnabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
-        self.init(
-            isEnabledProvider: {
-                userDefaults.getValue(for: isEnabledKey)
-            }
-        )
-    }
-    
-    // MARK: Public Instance Interface
-    
-    public func setIsDisabled(basedOn isDisabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
-        setIsDisabled {
-            userDefaults.getValue(for: isDisabledKey)
-        }
-    }
-    
-    public func setIsEnabled(basedOn isEnabledKey: UserDefaultsKey<Bool>, userDefaults: UserDefaults = .standard) {
-        setIsEnabled {
-            userDefaults.getValue(for: isEnabledKey)
         }
     }
 }
