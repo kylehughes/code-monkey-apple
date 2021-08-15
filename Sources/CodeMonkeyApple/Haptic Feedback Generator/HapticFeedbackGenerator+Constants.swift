9e04d89dbfbd3d1ffd4aa0870520fb5ae03ccfb6
Kyle Hughes <me@kylehugh.es>
2021-08-14 23:55:09 -0700

    Add semantic feedback constants for dismissing and presenting sheet

---
+++ Sources/CodeMonkeyApple/Haptic Feedback Generator/HapticFeedbackGenerator+Constants.swift
@@ -0,0 +1,11 @@
+//
+//  HapticFeedbackGenerator+Constants.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 8/14/21.
+//
+
+extension HapticFeedbackGenerator.SemanticFeedback {
+    public static let dismissSheet = Self(.impact(.light))
+    public static let presentSheet = Self(.impact(.medium))
+}
