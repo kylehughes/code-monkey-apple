713e0554e73efee95af1e8ea996a79c5eb01ec40
Kyle Hughes <me@kylehugh.es>
2021-08-14 23:39:19 -0700

    Add ReverseDomainName constants for all of my iOS apps

--- Sources/CodeMonkeyApple/Domain Name/ReverseDomainName.swift
+++ Sources/CodeMonkeyApple/Domain Name/ReverseDomainName.swift
@@ -17,9 +17,12 @@ public struct ReverseDomainName: Equatable, Hashable, Codable, RawRepresentable 
 extension ReverseDomainName {
     public static let es = ReverseDomainName(rawValue: "es")
     public static let es_computertechniqu = es.adding(subdomain: "computertechniqu")
+    public static let es_computertechniqu_rankThings = es_computertechniqu.adding(subdomain: "rank-things")
+    public static let es_computertechniqu_superHeadache = es_computertechniqu.adding(subdomain: "super-headache")
     public static let es_kylehugh = es.adding(subdomain: "kylehugh")
     public static let es_kylehugh_codeMonkey = es_kylehugh.adding(subdomain: "code-monkey")
     public static let es_kylehugh_codeMonkey_apple = es_kylehugh_codeMonkey.adding(subdomain: "apple")
+    public static let es_kylehugh_informationSuperhighway = es_kylehugh.adding(subdomain: "information-superhighway")
 }
 
 // MARK: - DomainNameProtocol Extension
