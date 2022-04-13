3ee4e6ca8bdf68807d67895a4d101d812808705d
Kyle Hughes <me@kylehugh.es>
2022-04-12 22:50:02 -0700

    Add built in user notification user info key

---
+++ Sources/CodeMonkeyApple/User Notifications/UserNotificationUserInfoKeys.swift
@@ -0,0 +1,10 @@
+//
+//  UserNotificationUserInfoKeys.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 4/12/22.
+//
+
+extension AnyHashable {
+    public static let dateRequestedUserNotificationUserInfoKey = "dateRequested_codeMonkeyApple"
+}
