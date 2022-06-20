4fd9c7ca23ff0b6df30b38f83692bdbb054c6d67
Kyle Hughes <me@kylehugh.es>
2022-06-20 16:54:22 -0700

    Add Device Did Shake View Modifier

---
+++ Sources/CodeMonkeyApple/SwiftUI/Device Did Shake View Modifier/DeviceDidShakeViewModifier.swift
@@ -0,0 +1,41 @@
+//
+//  DeviceDidShakeViewModifier.swift
+//  SuperHeadache
+//
+//  Created by Kyle Hughes on 8/21/21.
+//
+
+import SwiftUI
+import UIKit
+
+public struct DeviceShakeViewModifier: ViewModifier {
+    internal let action: () async -> Void
+    
+    // MARK: Public Instance Interface
+
+    public func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.deviceDidShakeNotification)) { _ in
+                Task {
+                    await action()
+                }
+            }
+    }
+}
+
+// MARK: - Extension for View
+
+extension View {
+    // MARK: Public Instance Interface
+    
+    public func onShake(perform action: @escaping () async -> Void) -> some View {
+        modifier(DeviceShakeViewModifier(action: action))
+    }
+}
+
+// MARK: - Extension for UIDevice
+
+extension UIDevice {
+    public static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
