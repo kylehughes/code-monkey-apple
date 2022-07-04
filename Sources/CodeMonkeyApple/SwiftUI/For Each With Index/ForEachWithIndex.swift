0f2684b523090a025c04d95524c572e1fae48fce
Kyle Hughes <me@kylehugh.es>
2022-07-04 00:56:19 -0700

    Implement ForEachWithIndex

---
+++ Sources/CodeMonkeyApple/SwiftUI/For Each With Index/ForEachWithIndex.swift
@@ -0,0 +1,43 @@
+//
+//  ForEachWithIndex.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 7/4/22.
+//
+
+import SwiftUI
+
+public struct ForEachWithIndex<Data, Content>
+where
+    Content: View,
+    Data: RandomAccessCollection,
+    Data.Index: Hashable
+{
+    private let content: (Data.Index, Data.Element) -> Content
+    private let data: Data
+    
+    // MARK: Public Initialization
+    
+    public init(_ data: Data, @ViewBuilder content: @escaping (Data.Element) -> Content) {
+        self.init(data) { _, element in
+            content(element)
+        }
+    }
+    
+    public init(_ data: Data, @ViewBuilder content: @escaping (Data.Index, Data.Element) -> Content) {
+        self.data = data
+        self.content = content
+    }
+}
+
+// MARK: - View Extension
+
+extension ForEachWithIndex: View {
+    // MARK: View Body
+    
+    public var body: ForEach<[(Data.Index, Data.Element)], Data.Index, Content> {
+        ForEach(Array(zip(data.indices, data)), id: \.0) { index, element in
+            content(index, element)
+        }
+    }
+}
