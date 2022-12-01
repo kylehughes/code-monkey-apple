93a17a97c1ade3c2d11d6f1e65e234adacbeb542
Kyle Hughes <me@kylehugh.es>
2022-11-30 19:52:24 -0800

    Add extension to conform UIContentSizeCategory to Storable

---
+++ Sources/CodeMonkeyApple/Storage/Extensions/UIContentSizeCategory+Storable.swift
@@ -0,0 +1,17 @@
+//
+//  UIContentSizeCategory+Storable.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 11/30/22.
+//
+
+#if canImport(UIKit)
+
+import Foundation
+import UIKit
+
+extension UIContentSizeCategory: Storable {
+    // NO-OP
+}
+
+#endif
