93bea745929006760b570b697c67dd9bfdd71618
Kyle Hughes <me@kylehugh.es>
2023-03-14 16:50:15 -0700

    Add HasHomeIndicator environment value

---
+++ Sources/CodeMonkeyApple/SwiftUI/Environment Values/HasHomeIndicatorEnvironmentValue.swift
@@ -0,0 +1,44 @@
+//
+//  HasHomeIndicatorEnvironmentValue.swift
+//  CodeMonkeyApple
+//
+//  Created by Kyle Hughes on 3/14/23.
+//
+
+import SwiftUI
+
+public struct HasHomeIndicatorKey {
+    // NO-OP
+}
+
+// MARK: - EnvironmentKey Extension
+
+extension HasHomeIndicatorKey: EnvironmentKey {
+    // MARK: Getting the Default Value
+    
+    public static var defaultValue: Bool {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return false
+        }
+        
+        let bottomSafeAreaInset = windowScene.windows.first?.safeAreaInsets.bottom ?? 0
+        
+        return bottomSafeAreaInset > 0
+    }
+}
+
+// MARK: - Extension for EnvironmentValues
+
+extension EnvironmentValues {
+    // MARK: Public Instance Interface
+    
+    public var hasHomeIndicator: Bool {
+        get {
+            self[HasHomeIndicatorKey.self]
+        }
+        set {
+            self[HasHomeIndicatorKey.self] = newValue
+        }
+    }
+}
+
