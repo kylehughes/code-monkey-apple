e07111729273c920880514cdea6ebd2df5c19431
Kyle Hughes <me@kylehugh.es>
2025-01-11 18:15:06 -0800

    Add `zero` constant for EdgeInsets

---
+++ Sources/CodeMonkeyApple/Extensions/SwiftUI/EdgeInsets+Zero.swift
@@ -0,0 +1,14 @@
+//
+//  EdgeInsets+Zero.swift
+//  code-monkey-apple
+//
+//  Created by Kyle Hughes on 1/11/25.
+//
+
+import SwiftUI
+
+extension EdgeInsets {
+    // MARK: Public Static Interface
+    
+    public static let zero = EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+}
